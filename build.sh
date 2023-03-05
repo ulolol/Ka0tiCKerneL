e56e45993092b091430dd2020aea479ad8695b28
ulolol <ulolollyfz@gmail.com>
2023-03-05 17:57:56 +0530

    Set Video Mode, protected mode and x64 mode

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 nasm -f bin -o boot.bin boot.asm
 nasm -f bin -o loader.bin loader.asm
-dd if=boot.bin of=boot.img bs=512 count=1 conv=notrunc
-dd if=loader.bin of=boot.img bs=512 count=5 seek=1 conv=notrunc
+dd if=boot.bin of=kaosboot.img bs=512 count=1 conv=notrunc
+dd if=loader.bin of=kaosboot.img bs=512 count=5 seek=1 conv=notrunc
